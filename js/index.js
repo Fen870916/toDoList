8e508999cb51fc2dad58fc3478e54e54cabb9480
fen <a0903169366@gmail.com>
2022-02-10 03:48:37 +0800

    改變外觀

--- js/index.js
+++ js/index.js
@@ -7,17 +7,14 @@ function clickAddButton() {
   if (document.querySelector('.enter').value !== '') {
     let matter = document.createElement('div')
     matter.classList.add('matter')
-    let number = document.querySelectorAll('.matter').length
     matter.innerHTML =
-      `<div class="number">` +
-      (number + 1) +
-      `</div>
-      <button class="status undone">X</button>
+      `
+      <button class="status undone"><i class="fas fa-times"></i></button>
             <div class="content">` +
       document.querySelector('.enter').value +
       `</div>
-                <button class="edit">edit</button>
-                <button class="delete">delete</button>`
+                <button class="delete"><i class="far fa-trash-alt"></i></button>`
+
     if (document.querySelector('.list').classList.contains('complete')) {
       matter.style.display = 'none'
     }
@@ -32,31 +29,29 @@ function clickDeleteButton(e) {
   for (i = 0; i < document.querySelectorAll('.delete').length; i++) {
     if (e.target === document.querySelectorAll('.delete')[i]) {
       e.target.parentNode.parentNode.removeChild(e.target.parentNode)
-
-      // 讓number不會有跳號
-      for (i = 0; i < document.querySelectorAll('.number').length; i++) {
-        document.querySelectorAll('.number')[i].innerHTML = i + 1
-      }
+    } else if (e.target === document.querySelectorAll('.delete>i')[i]) {
+      e.target.parentNode.parentNode.parentNode.removeChild(
+        e.target.parentNode.parentNode
+      )
     }
   }
 }
 
 // 編輯matter
 
-document.addEventListener('click', clickEditButton)
-function clickEditButton(e) {
+document.addEventListener('click', clickContentButton)
+function clickContentButton(e) {
   let flag
   let number
-  for (i = 0; i < document.querySelectorAll('.edit').length; i++) {
+  for (i = 0; i < document.querySelectorAll('.content').length; i++) {
     if (
-      e.target === document.querySelectorAll('.edit')[i] &&
+      e.target === document.querySelectorAll('.content')[i] &&
       e.target.parentNode.querySelector('.status').classList.contains('undone')
     ) {
       // 按下edit後被編輯的事項會有顏色上的變化
       e.target.parentNode.style.backgroundColor = 'red'
       document.querySelector('.enter').focus()
-      document.querySelector('.enter').value =
-        e.target.parentNode.querySelector('.content').innerHTML
+      document.querySelector('.enter').value = e.target.innerHTML
       number = i
       flag = 1
     }
@@ -66,7 +61,7 @@ function clickEditButton(e) {
         // 取消或完成編輯後編輯的事項會恢復原有的顏色
         document.querySelectorAll('.content')[
           number
-        ].parentNode.style.backgroundColor = 'transparent'
+        ].parentNode.style.backgroundColor = 'wheat'
         document.querySelectorAll('.content')[number].innerHTML = e.target.value
         e.target.value = ''
         flag = 0
@@ -83,11 +78,25 @@ function clickStatusButton(e) {
       e.target === document.querySelectorAll('.status')[i] &&
       e.target.classList.contains('undone')
     ) {
-      e.target.innerHTML = `O`
       e.target.classList.remove('undone')
       e.target.classList.add('complete')
       e.target.parentNode.querySelector('.content').innerHTML =
         e.target.parentNode.querySelector('.content').innerText.strike()
+      e.target.innerHTML = `<i class="fas fa-check"></i>`
+      if (!document.querySelector('.list').classList.contains('all')) {
+        clickUndoneButton()
+      }
+    } else if (
+      e.target === document.querySelectorAll('.status>i')[i] &&
+      e.target.parentNode.classList.contains('undone')
+    ) {
+      e.target.parentNode.classList.remove('undone')
+      e.target.parentNode.classList.add('complete')
+      e.target.parentNode.parentNode.querySelector('.content').innerHTML =
+        e.target.parentNode.parentNode
+          .querySelector('.content')
+          .innerText.strike()
+      e.target.parentNode.innerHTML = `<i class="fas fa-check"></i>`
       if (!document.querySelector('.list').classList.contains('all')) {
         clickUndoneButton()
       }
@@ -95,11 +104,23 @@ function clickStatusButton(e) {
       e.target === document.querySelectorAll('.status')[i] &&
       e.target.classList.contains('complete')
     ) {
-      e.target.innerHTML = `X`
       e.target.classList.remove('complete')
       e.target.classList.add('undone')
       e.target.parentNode.querySelector('.content').innerHTML =
         e.target.parentNode.querySelector('.content').innerText
+      e.target.innerHTML = `<i class="fas fa-times">`
+      if (!document.querySelector('.list').classList.contains('all')) {
+        clickCompleteButton()
+      }
+    } else if (
+      e.target === document.querySelectorAll('.status>i')[i] &&
+      e.target.parentNode.classList.contains('complete')
+    ) {
+      e.target.parentNode.classList.remove('complete')
+      e.target.parentNode.classList.add('undone')
+      e.target.parentNode.parentNode.querySelector('.content').innerHTML =
+        e.target.parentNode.parentNode.querySelector('.content').innerText
+      e.target.parentNode.innerHTML = `<i class="fas fa-times">`
       if (!document.querySelector('.list').classList.contains('all')) {
         clickCompleteButton()
       }
@@ -125,7 +146,6 @@ document
   .querySelector('.sort>.complete')
   .addEventListener('click', clickCompleteButton)
 function clickCompleteButton() {
-  console.log(1)
   for (i = 0; i < document.querySelectorAll('.matter>.status').length; i++) {
     if (
       !document
